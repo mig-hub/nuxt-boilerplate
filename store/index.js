902c3a120db44c9519451b5bf7c456744071e937
Mickael Riga <mickael.riga.79@gmail.com>
2022-01-10 11:41:38 +0100

    Add store boilerplate

---
+++ store/index.js
@@ -0,0 +1,49 @@
+// Uncomment if using contentful plugin.
+// Then uncomment and modify the specific lines in actions.
+// import contentfulClient from '~/plugins/contentful.js'
+//
+export const state = () => ({
+  contentLoaded: false
+})
+
+export const getters = {
+}
+
+export const mutations = {
+
+  setContentLoaded(state, bool) {
+    state.contentLoaded = (typeof bool != 'boolean') ? true : bool
+  },
+
+  // Try to use this generic one only when the key is dynamic.
+  // For example when looping through contentful pages.
+  // Otherwise it is better to use a specific mutator instead.
+  setKeyValue(state, key, value) {
+    state[key] = value
+  }
+
+}
+
+export const actions = {
+
+  async initState({ commit }) {
+
+    // Uncomment for contentful.
+    // Also uncomment the plugin load in config and the import at the top of this file.
+
+    // let content = await contentfulClient.getEntries({
+    //   'sys.contentType.sys.id[in]': 'homePage',
+    //   include: 10,
+    //   limit: 5
+    // })
+    // content.items.forEach(function(item) {
+    //   commit('setKeyValue', item.sys.contentType.sys.id, item)
+    // })
+
+    //
+
+    commit('setContentLoaded')
+  }
+
+}
+
