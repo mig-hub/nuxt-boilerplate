3f8d790f9d22972cbf9cce7817871a7c05e7fffc
Mickael Riga <mickael.riga.79@gmail.com>
2022-04-01 15:21:19 +0200

    Add ContentfulImg component

---
+++ components/ContentfulImg.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+img(v-bind:src='imgSrc' v-bind:alt='imgAlt' v-bind:title='imgTitle')
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: Object
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    imgSrc() {
+      return this.image.fields.file.url
+    },
+    imgAlt() {
+      return this.alt || this.image.fields.title
+    },
+    imgTitle() {
+      return this.title
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
