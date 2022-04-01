88bc25c9d283b482b76fdf990f9738e4341da5f6
Mickael Riga <mickael.riga.79@gmail.com>
2022-04-01 14:22:56 +0200

    Add smoothscroll polyfill

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {src: '~/plugins/polyfills.js', mode: 'client'},
     // '~/plugins/contentful.js',
     // '~/plugins/prevNextPageTransition.js',
     '~/plugins/framework.js'
